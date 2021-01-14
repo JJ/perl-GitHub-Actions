bdbd12a274cb0a345060d57b41464d7714d77348
JJ Merelo <jjmerelo@gmail.com>
2021-01-14 12:01:26 +0100

    Preparing for release

--- lib/GitHub/Actions.pm
+++ lib/GitHub/Actions.pm
@@ -40,114 +40,97 @@ __END__
 
 =head1 NAME
 
-GitHub::Actions - [One line description of module's purpose here]
+GitHub::Actions - Work in GitHub Actions using Perl
 
 
 =head1 VERSION
 
-This document describes GitHub::Actions version 0.0.1
+This document describes GitHub::Actions version 0.0.3
 
 
 =head1 SYNOPSIS
 
     use GitHub::Actions;
+    use v5,14;
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
+    # %github contains all GITHUB_* environment variables
+    for my $g (keys %github ) {
+       say "GITHUB_$g -> ", $github{$g}
+    }
+
+    # Set step output
+    set_output("FOO", "BAR");
+
+    # Set environment variable value
+    set_env("FOO", "BAR");
+
+Install this action within a GitHub action
+
+      . name: "Install GitHub::Actions"
+        run: sudo cpan GitHub::Actions
+
+(we need C<sudo> since we're using the system Perl)
+
+You can use this as a C<step>
+
+      - name: Test env variables
+        shell: perl {0}
+        run: |
+          use GitHub::Actions;
+          set_env( 'FOO', 'BAR');
+
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+GitHub Actions include, by default, at least in its linux runners, a
+system Perl which you can use directly in your GitHub actions. This is
+a (for the time being) minimalistic module that tries to help a bit
+with that, by defining a few functions that will be useful when
+performing GitHub actions. Besides the system Perl, you can use any of
+L<the modules
+installed|https://gist.github.com/JJ/edf3a39d68525439978da2a02763d42b>. You
+can install other modules via cpan or, preferably for speed, via the
+Ubuntu package (or equivalent)
 
+Check out an example of using it in the L<repository|https://github.com/JJ/perl-GitHub-Actions/blob/main/.github/workflows/self-test.yml>
 
 =head1 INTERFACE 
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+=head2 set_env( $env_var_name, $env_var_value)
 
+This is equivalent to L<setting an environment variable|https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable>
 
-=head1 DIAGNOSTICS
+=head2 set_output( $output_name, $output_value)
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
+Equivalent to L<C<set_output>|https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter>
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-GitHub::Actions requires no configuration files or environment variables.
+GitHub::Actions requires no configuration files or environment
+variables. Those set by GitHub Actions will only be available there,
+or if you set them explicitly. Remember that they will need to be set
+during the C<BEGIN> phase to be available when this module loads.
+
+    BEGIN {
+      $ENV{'GITHUB_FOO'} = 'foo';
+      $ENV{'GITHUB_BAR'} = 'bar';
+    }
 
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
+Intentionally, no dependencies are included.
 
 
 =head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
 
 None reported.
 
 
 =head1 BUGS AND LIMITATIONS
 
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<bug-github-actions@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
+Please report any bugs or feature requests to L<https://github.com/JJ/perl-GitHub-Actions/issues>.
 
 
 =head1 AUTHOR
